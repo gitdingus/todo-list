90fec06152f8e00ded951cf33b9674b38a47b1d5
Josh S <aaabccc87@protonmail.com>
2022-06-20 14:57:51 -0400

    Added behavoir and style

--- src/addProjectWidget.js
+++ src/addProjectWidget.js
@@ -1,4 +1,5 @@
 import { createHtmlElement } from 'dom-utils';
+import './addProjectWidget.css';
 import plusIcon from './icons/plus-thick.svg';
 import minusIcon from './icons/minus-thick.svg';
 
@@ -50,14 +51,70 @@ function createAddProjectWidget(){
         ],
     });
 
-    const addProjectButton = addProjectWidget.querySelector("#add-project-button");
-    const cancelProjectButton = addProjectWidget.querySelector("#cancel-add-project-button");
+    const _addProjectEvents = [];
+    const _addProjectText = addProjectWidget.querySelector("#add-project-text");
+    const _addProjectButton = addProjectWidget.querySelector("#add-project-button");
+    const _cancelProjectButton = addProjectWidget.querySelector("#cancel-add-project-button");
 
-    function disableButtons(){
+    _disableButtons();
 
+    _addProjectText.addEventListener("focus", _enableButtons);
+    _addProjectText.addEventListener("blur", _disableButtons);
+
+    _addProjectButton.addEventListener("click", _addProject)
+    _cancelProjectButton.addEventListener("click", _cancelInput);
+
+    function _disableButtons(){
+        if (_addProjectText.value === ""){
+            _addProjectButton.disabled = true;
+            _addProjectButton.classList.add("disabled");
+
+            _cancelProjectButton.disabled = true;
+            _cancelProjectButton.classList.add("disabled");
+
+            _addProjectText.classList.add("inactive");
+        }
 
     }
-    return addProjectWidget;
+
+    function _enableButtons(){
+        _addProjectButton.disabled = false;
+        _addProjectButton.classList.remove("disabled");
+
+        _cancelProjectButton.disabled = false;
+        _cancelProjectButton.classList.remove("disabled");
+
+        _addProjectText.classList.remove("inactive");
+    }
+
+    function _inputBlur(){
+        _disableButtons();
+    }
+
+    function _inputFocus(){
+        _enableButtons();
+    }
+
+    function _cancelInput(){
+        _addProjectText.value = "";
+        _disableButtons();
+    }
+
+    function _addProject(){
+        _addProjectEvents.forEach( fn => fn() );
+
+        _addProjectText.value = "";
+        _disableButtons();
+    }
+
+    function addSubmitAction(fn){
+        _addProjectEvents.push(fn);
+    }
+
+    function getValue(){
+        return _addProjectText.value;
+    }
+    return { addProjectWidget, addSubmitAction, getValue };
 }
 
 export { createAddProjectWidget }
