08f5da366a1e9740d54eb959f50448721b6bf4db
Josh S <aaabccc87@protonmail.com>
2022-06-22 18:16:28 -0400

    Modify rule for html

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                 use: [ "style-loader", "css-loader" ],
             },
             {
-                test: /new\-todo\-form\.html$/i,
+                test: /\.html$/i,
                 type: "asset/source",
             }
         ],
