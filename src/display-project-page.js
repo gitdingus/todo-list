cfe9ac0b53f203ff4b02956fb156bb3255829d38
Josh S <aaabccc87@protonmail.com>
2022-06-23 09:21:59 -0400

    Display project and it's todo list

---
+++ src/display-project-page.js
@@ -0,0 +1,63 @@
+import { createHtmlElement } from 'dom-utils';
+import { createCollapsedNoteWidget } from './note-widget-collapsed';
+import { EventEmitter }from 'event-emitter'
+import './display-project-page.css';
+import plusImage from './icons/plus-thick.svg';
+
+function createProjectPage(project){
+    const events = EventEmitter;
+    const _project = project;
+    const projectElement = createHtmlElement({
+        tag: "div",
+        properties: {
+            id: "project-panel",
+        },
+        children: [
+            createHtmlElement({
+                tag: "div",
+                properties: {
+                    id: "project-bar",
+                },
+                children: [
+                    createHtmlElement({
+                        tag: "h1",
+                        classes: [ "project-name" ],
+                        properties: {
+                            textContent: _project.getName(),
+                        },
+                    }),
+                    createHtmlElement({
+                        tag: "button",
+                        classes: [ "add-todo-button" ],
+                        children: [
+                            createHtmlElement({
+                                tag: "img",
+                                attributes: {
+                                    src: plusImage,
+                                },
+                            }),
+                        ],
+                    }),
+                ],
+            }),
+            createHtmlElement({
+                tag: "div",
+                classes: [ "todo-display-panel" ],
+            }),
+        ],
+    });
+
+    const addTodoButton = projectElement.querySelector(".add-todo-button");
+    const displayPanel = projectElement.querySelector(".todo-display-panel");
+
+    _project.getTodos().forEach( todo => displayPanel.appendChild(createCollapsedNoteWidget(todo).collapsedNoteWidgetElement) );
+    addTodoButton.addEventListener("click", raiseEvent);
+    
+    function raiseEvent(e){
+        events.raiseEvent("newTodo", _project); // pass the project to add a todo to
+    }
+
+    return projectElement;
+}
+
+export { createProjectPage }
