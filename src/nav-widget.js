ae5faed9f747bd84e26b69a19042b94e00b2a6a9
Josh S <aaabccc87@protonmail.com>
2022-06-23 22:46:40 -0400

    Experiment with showing history hierarchy with linked list

--- src/nav-widget.js
+++ src/nav-widget.js
@@ -10,8 +10,90 @@ function createNavWidget(){
     const _projects = { };
     const _project = {  };
     const _todo = {};
+
+    let _chain = { };
+    /*
+     chain has 
+        displayText: text to display in nav bar
+        link: a copy of the element that was displayed when it was added 
+                so it may return to that element when its clicked
+        nextLink: a link to the next object in the chain
+    
+    */
     // const todo = {type: "todo", createDiv: , todo:};
     
+    /*
+        looking at current link in chain
+        if current link does not have next link,
+        make new link equal to current link.nextlink
+
+        else if current link does have nextlink
+            call function with next link, and link to add
+    */
+    function addToNavBar(type, displayText, div){
+        const link = { type, displayText, div };
+        _addLink(_chain, link);
+    }
+
+    function initializeNavBar(type, displayText, div){
+        _chain = { type, displayText, div, nextLink: {} };
+        _buildChain(_chain);
+    }
+
+    function _addLink(currentLink, newLink){
+
+        if (currentLink.nextLink.type === newLink.type){
+            newLink.nextLink = {};
+            currentLink.nextLink = newLink;
+
+            _clearNav();
+            _buildChain(_chain);
+        }
+        else if (isEmpty(currentLink.nextLink)){
+            currentLink.nextLink = newLink;
+            newLink.nextLink = {};
+
+            _clearNav();
+            _buildChain(_chain);
+        }
+        else{
+            _addLink(currentLink.nextLink, newLink);
+        
+        }
+    }
+    
+    function _buildChain(chain){
+        if (!isEmpty(chain)){
+            navWidget.appendChild(_getLinkElement(chain));
+        }
+
+        if (!isEmpty(chain.nextLink)){
+            navWidget.appendChild(_getArrow());
+            _buildChain(chain.nextLink);
+        }
+    }
+
+    function _getLinkElement(link){
+        const element = createHtmlElement({
+            tag: "p",
+            properties: {
+                textContent: link.displayText,
+            },
+        });
+        element.addEventListener("click", function (){
+            link.nextLink = {};
+            _clearNav();
+            _buildChain(_chain);
+            _displayDiv(link.div);
+        });
+
+        return element;
+    }
+
+    function _displayDiv(div){
+        _clearMainContent();
+        mainContent.appendChild(div);
+    }
 
     function setProjectsPage(div){
         _projects.div = div;
@@ -45,7 +127,7 @@ function createNavWidget(){
         return element;
     }
 
-    function getArrow(){
+    function _getArrow(){
         const arrow = createHtmlElement({
             tag: "p",
             properties: {
@@ -56,13 +138,13 @@ function createNavWidget(){
         return arrow;
     }
 
-    function clearNav(){
+    function _clearNav(){
         while (navWidget.firstChild){
             navWidget.firstChild.remove();
         }
     }
 
-    function clearMainContent(){
+    function _clearMainContent(){
         while (mainContent.firstChild){
             mainContent.firstChild.remove();
         }
@@ -72,8 +154,7 @@ function createNavWidget(){
         clearNav();
 
         navWidget.appendChild(getNavElement("projects", "projects"));
-console.log(_project.project);
-console.log(_project.project == true);
+
         if (!isEmpty(_project.project)){
             navWidget.appendChild(getArrow());
             navWidget.appendChild(getNavElement("project", _project.project.getName()));
@@ -82,7 +163,6 @@ console.log(_project.project == true);
     }
 
     function navItemClicked(e){
-        console.log(e.target.id);
         if (e.target.id === "projects"){
             console.log("clicked projects");
             setActiveTodo({});
@@ -117,11 +197,8 @@ console.log(_project.project == true);
     }
 
     return {
-        navWidget, 
-        updateNav,
-        setActiveProject, 
-        setProjectsPage,
-        getActiveProject 
+        addToNavBar,
+        initializeNavBar
     }
 }
 
